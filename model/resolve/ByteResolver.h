387419381e500ff86854d394b1811cd773bc275f
ChasonDeshotel <me@chasondeshotel.com>
2026-01-29 13:47:13 -0600

    wip fixup from TArray/FString refactor

--- model/resolve/ByteResolver.h
+++ model/resolve/ByteResolver.h
@@ -13,22 +13,22 @@ struct ByteResolver {
             out.kind = ResolvedValue::Kind::Enum;
             out.uEnum = enumMaybe;
             if (raw < enumMaybe->Names.size()) {
-                const FName& name = enumMaybe->Names[raw];
-                if (!name.FNameEntryId) {
-                    out.invalid = true;
-                    return;
-                }
-                out.primitiveStr = "FNameEntryId: " + std::to_string(name.FNameEntryId) + "InstanceNumber: " + std::to_string(name.InstanceNumber);
-                // fixme engine gate
-                //{
-                //    const auto nameEntry = Runtime::getFNameEntry(name.FNameEntryId);
-                //    if (!nameEntry) {
-                //        out.mInvalid = true;
-                //        return;
-                //    }
-                //    const std::wstring string(nameEntry->Name);
-                //    out.primitiveStr = string_tool::enshrinken(string);
+                //const FName& name = enumMaybe->Names[raw];
+                //if (!name.FNameEntryId) {
+                //    out.invalid = true;
+                //    return;
                 //}
+                //out.primitiveStr = "FNameEntryId: " + std::to_string(name.FNameEntryId) + "InstanceNumber: " + std::to_string(name.InstanceNumber);
+                //// fixme engine gate
+                ////{
+                ////    const auto nameEntry = Runtime::getFNameEntry(name.FNameEntryId);
+                ////    if (!nameEntry) {
+                ////        out.mInvalid = true;
+                ////        return;
+                ////    }
+                ////    const std::wstring string(nameEntry->Name);
+                ////    out.primitiveStr = string_tool::enshrinken(string);
+                ////}
             } else {
                 out.invalid = true;
                 out.primitiveStr =
